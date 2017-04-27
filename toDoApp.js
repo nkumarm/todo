95f08db43aad6cfd370b5b3030c4582dd08a5939
Nanda Kumar M <nandamut@cisco.com>
2017-04-27 10:43:38 +0530

    added package and server files

---
+++ toDoApp.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var toDoApp = express();
+
+toDoApp.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+toDoApp.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
